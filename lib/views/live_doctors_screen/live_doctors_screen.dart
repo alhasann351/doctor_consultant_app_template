89cc856e31a90e56d49675afba91078f4d19efce
alhasann351 <alhasann352@gmail.com>
2024-08-06 20:11:29 +0600

    live-background-set

--- lib/views/live_doctors_screen/live_doctors_screen.dart
+++ lib/views/live_doctors_screen/live_doctors_screen.dart
@@ -7,7 +7,18 @@ class LiveDoctorsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Image.asset(Get.arguments['image'.toString()])),
+      body: Stack(
+        children: [
+          SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: Image.asset(
+              Get.arguments['image'.toString()],
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
